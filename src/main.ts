a4820833460a34a150a30d56e052999a01f280ec
Michael Abah <donsmartez@gmail.com>
2021-06-30 12:07:37 +0100

    fixing heroku server path

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ dotenv.config()
 async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
